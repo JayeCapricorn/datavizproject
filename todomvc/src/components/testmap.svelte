464759462444c5ae1eb385db7bb394f499d60377
Peiqi Chen <peiqichen0102@gmail.com>
2023-04-22 11:13:39 -0400

    Adding mapbox to front page

--- todomvc/src/components/testmap.svelte
+++ todomvc/src/components/testmap.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
   import { json } from "d3";
   import Marks from "./Marks.svelte";
   let dataset = [];
@@ -16,4 +16,98 @@
   <svg {width} {height}>
     <Marks {dataset} />
   </svg>
-</main>
+</main> -->
+
+  <script>
+    import mapboxgl from "mapbox-gl";
+    import { onMount } from "svelte";
+    export let geoJsonToFit;
+  
+    mapboxgl.accessToken =
+    "pk.eyJ1IjoicGVpcWljIiwiYSI6ImNsZ2JseW13ZDA0ejUzcG85ZmJhN3RndXUifQ.Tbnw2SnV2zigv50y8sZerQ";
+  
+    let container;
+    let map;
+  
+    let zoomLevel;
+  
+    function updateZoomLevel() {
+      const screenWidth = window.innerWidth;
+      zoomLevel = screenWidth <= 600 ? 4 : 5.85; // Adjust these values as needed
+    }
+  
+    function handleResize() {
+      updateZoomLevel();
+      map.setZoom(zoomLevel);
+    }
+  
+    onMount(() => {
+      updateZoomLevel();
+      map = new mapboxgl.Map({
+        container,
+        style: "mapbox://styles/mapbox/light-v11",
+        center: [-85.6024, 12.7690],
+        zoom: zoomLevel,
+        attributionControl: true, // removes attribution from the bottom of the map
+      });
+  
+      window.addEventListener("resize", handleResize);
+  
+      // function hideLabelLayers() {
+      //   const labelLayerIds = map
+      //     .getStyle()
+      //     .layers.filter(
+      //       (layer) =>
+      //         layer.type === "symbol" && /label|text|place/.test(layer.id)
+      //     )
+      //     .map((layer) => layer.id);
+  
+      //   for (const layerId of labelLayerIds) {
+      //     map.setLayoutProperty(layerId, "visibility", "none");
+      //   }
+      // }
+  
+      map.on("load", () => {
+        // hideLabelLayers();
+        updateBounds();
+        map.on("zoom", updateBounds);
+        map.on("drag", updateBounds);
+        map.on("move", updateBounds);
+      });
+    });
+    
+    function updateBounds() {
+      const bounds = map.getBounds();
+      geoJsonToFit.features[0].geometry.coordinates = [
+        bounds._ne.lng,
+        bounds._ne.lat,
+      ];
+      geoJsonToFit.features[1].geometry.coordinates = [
+        bounds._sw.lng,
+        bounds._sw.lat,
+      ];
+    }
+  
+  </script>
+  
+  <svelte:head>
+    <link
+      rel="stylesheet"
+      href="https://api.mapbox.com/mapbox-gl-js/v2.14.0/mapbox-gl.css"
+    />
+  </svelte:head>
+  
+  <div class="map" bind:this={container} />
+  
+  <style>
+    .map {
+      width: 90%;
+      height: 90%; /* check problem when setting width */
+      position: absolute;
+      transition: opacity 2s, visibility 2s;
+      /* outline: blue solid 3px; */
+    }
+  
+  </style>
+  
+  
--- todomvc/src/routes/frontpage/+page.svelte
+++ todomvc/src/routes/frontpage/+page.svelte
@@ -1,12 +1,17 @@
 <script>
     import Navbar from "../../components/Navbar.svelte";
+    import Testmap from "../../components/testmap.svelte";
 
 </script>
 
 <Navbar />
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+    <Testmap />
+</div>
+
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
 
 <!-- <main>
     <a href="/">View Timeline</a>
