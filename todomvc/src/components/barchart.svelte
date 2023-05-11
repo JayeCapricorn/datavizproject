9ef2ee85420156e9ebf2fd28c9465a7979df3df0
Zhaofeng Wu <wuzhaofeng1997@gmail.com>
2023-05-10 23:06:01 -0400

    tune bar chart

--- todomvc/src/components/barchart.svelte
+++ todomvc/src/components/barchart.svelte
@@ -19,18 +19,29 @@
 	$: yScale = d3
 		.scaleBand()
 		.domain(data.map((d) => d.type))
-		.range([margin.top, height - margin.bottom]);
+		.range([margin.top, height - margin.bottom])
+		.padding(0.5);
 
 	let xAxis;
 	let yAxis;
 
 	$: {
 		d3.select(xAxis).call(d3.axisBottom(xScale));
-		d3.select(yAxis).call(d3.axisLeft(yScale));
+		d3.select(yAxis)
+			.call(d3.axisLeft(yScale).tickSize(0).tickPadding(10))
+			.selectAll(".domain, .tick line")
+			.style("stroke", "none");
 	}
 
 	const centerX = width / 2;
 	const centerY = height / 2;
+	let has_negative = false;
+	data.forEach((d) => {
+		if (d.value < 0) {
+			has_negative = true;
+		}
+	});
+	let right_shift = has_negative ? 40 : 0;
 </script>
 
 <svg {width} {height} viewBox={`0 0 ${width} ${height}`}>
@@ -41,11 +52,13 @@
 				? centerX -
 				  (width - margin.left - margin.right) / 2 +
 				  xScale(0) -
-				  margin.left
+				  margin.left +
+				  right_shift
 				: centerX -
 				  (width - margin.left - margin.right) / 2 +
 				  xScale(d.value) -
-				  margin.left}
+				  margin.left +
+				  right_shift}
 			y={centerY -
 				(height - margin.top - margin.bottom) / 2 +
 				yScale(d.type) -
@@ -56,13 +69,36 @@
 			height={yScale.bandwidth()}
 			fill={d.highlight ? "red" : "blue"}
 		/>
+		<text
+			x={d.value > 0
+				? centerX -
+				  (width - margin.left - margin.right) / 2 -
+				  margin.left +
+				  xScale(d.value) +
+				  right_shift +
+				  10
+				: centerX -
+				  (width - margin.left - margin.right) / 2 +
+				  xScale(d.value) -
+				  margin.left -
+				  (d.value + "").length * 10 +
+				  right_shift +
+				  10}
+			y={centerY -
+				(height - margin.top - margin.bottom) / 2 +
+				yScale(d.type) -
+				margin.top +
+				yScale.bandwidth() / 2 +
+				5}
+			fill={d.highlight ? "red" : "blue"}>{d.value}</text
+		>
 	{/each}
 	<!-- <g transform={`translate(0, ${centerY + (height - margin.top - margin.bottom) / 2})`}
 		 bind:this={xAxis} /> -->
 	<g
 		transform={`translate(${
 			centerX - (width - margin.left - margin.right) / 2
-		}, 0)`}
+		}, 5)`}
 		bind:this={yAxis}
 	/>
 </svg>
